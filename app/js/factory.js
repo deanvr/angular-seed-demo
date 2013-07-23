1c76002c6ed03da8c12c1f38d035b37325e971d1
whitelockr <robert.whitelock@gmail.com>
2013-07-23 09:32:25 +0100

    animations

--- app/js/factory.js
+++ app/js/factory.js
@@ -1,7 +1,7 @@
 angular.module('FG.Alerts.Factory', []).factory('getAlerts',function($http){
     var factory = {}
     factory.fleets = function(){
-            return $http.get('http://flight.apiary.io/v1/Fleet/Alerts?apiToken=00000000-0000-0000-0000-000000000001&skip=0&take=7')
+            return $http.get('http://api.flightgloballocal.rbidev.ds/v1/Fleet/Alerts?apiToken=4C959CCF-6F55-466A-A296-125248EE8DD7&skip=0&take=50')
     };
     return factory;
 });
